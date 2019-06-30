ee229b89f8b118337845fe4921fc0a195fa67d69
luxiaobing <luxiaobing2014@163.com>
2019-06-30 21:01:36 +0800

    更新文件

--- src/main/java/com/lxb/service/impl/UserInfoServiceImpl.java
+++ src/main/java/com/lxb/service/impl/UserInfoServiceImpl.java
@@ -3,8 +3,7 @@ package com.lxb.service.impl;
 
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.stereotype.Service;
 import com.lxb.api.UserService;
 
