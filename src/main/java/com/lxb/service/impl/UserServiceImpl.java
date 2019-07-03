54f36e380714da20981889450e50c9a85181f895
luxiaobing <luxiaobing2014@163.com>
2019-07-04 00:12:13 +0800

    测试dubbo接口

---
+++ src/main/java/com/lxb/service/impl/UserServiceImpl.java
@@ -0,0 +1,25 @@
+package com.lxb.service.impl;
+
+
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.stereotype.Service;
+import com.lxb.api.UserService;
+
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author Xiaobing.Lu
+ * @create 2019-05-30 23:28
+ **/
+@Slf4j
+@Service("userService")
+public class UserServiceImpl implements UserService {
+    public String queryUserCodeByDubbo(String userCode){
+        log.info("queryUserCodeByDubbo,userCode:{}", userCode);
+        return userCode + "aa";
+    }
+
+}
